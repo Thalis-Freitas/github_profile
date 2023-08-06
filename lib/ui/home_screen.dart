caa3283ed74ce1978322024215f08e0db632a85d
Thalis Freitas <ofthalis@gmail.com>
2023-08-06 19:04:40 -0300

    refactor HomeScreen and ResultsScreen

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -9,67 +9,84 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          _buildHeader(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                Container(height: 30.0),
+                _buildUsernameTextField(),
+                Container(height: 10.0),
+                _buildSearchButton(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      width: double.maxFinite,
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(120)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Image.asset(
+          'assets/images/icon_github.png',
+          height: 120,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUsernameTextField() {
     const outlineBorder = OutlineInputBorder(
       borderSide: BorderSide(
         color: Colors.black,
       ),
     );
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          Container(
-            width: double.maxFinite,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(120))
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(50),
-              child: Image.asset(
-                'assets/images/icon_github.png',
-                height: 120,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Container(height: 30.0),
-                TextField(
-                  controller: _username,
-                  decoration: const InputDecoration(
-                    border: outlineBorder,
-                    focusedBorder: outlineBorder,
-                    labelText: 'username',
-                    labelStyle: TextStyle(color: Colors.black),
-                  ),
-                ),
-                Container(height: 10.0),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: const MaterialStatePropertyAll(Colors.black),
-                    minimumSize: MaterialStateProperty.all(const Size(double.maxFinite, 50)),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context, MaterialPageRoute(
-                        builder: (context) => ResultsScreen(username: _username.value.text)
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Buscar',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+    return TextField(
+      controller: _username,
+      decoration: const InputDecoration(
+        border: outlineBorder,
+        focusedBorder: outlineBorder,
+        labelText: 'username',
+        labelStyle: TextStyle(color: Colors.black),
       ),
+    );
+  }
+
+  Widget _buildSearchButton(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.black),
+        minimumSize:
+            MaterialStateProperty.all(const Size(double.maxFinite, 50)),
+      ),
+      onPressed: () {
+        _navigateToResultsScreen(context);
+      },
+      child: const Text(
+        'Buscar',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    );
+  }
+
+  void _navigateToResultsScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ResultsScreen(username: _username.value.text)),
     );
   }
 }
--- lib/ui/results_screen.dart
+++ lib/ui/results_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'dart:convert'; 
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'package:github_profile/models/user.dart';
 
 class ResultsScreen extends StatelessWidget {
   final String username;
+
   const ResultsScreen({super.key, required this.username});
 
   @override
@@ -18,75 +19,85 @@ class ResultsScreen extends StatelessWidget {
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: FutureBuilder(
-          future: _fetchUser(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final user = snapshot.data;
-              return Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.network(user!.avatarUrl, height: 100,),
-                    ),
-                    Container(height: 10),
-                    Text(user.name, style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    )),
-                    Container(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                          decoration: const BoxDecoration(
-                            color: Colors.purple,
-                          ),
-                          child: Text("Seguidores: ${user.followers}",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Container(width: 16),
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                          decoration: const BoxDecoration(
-                            color: Colors.deepOrange,
-                          ),
-                          child: Text("Repositórios: ${user.publicRepos}",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            } else if(snapshot.hasError) {
-              return Text("Erro: ${snapshot.error}");
-            }
-
-            return const CircularProgressIndicator(
-              backgroundColor: Colors.black,
-            );
-          },
-        ),
+        child: _buildUserDetailsOrLoading(),
       ),
     );
   }
 
-  Future<User> _fetchUser() async {
+  Widget _buildUserDetailsOrLoading() {
+    return FutureBuilder(
+      future: _fetchUser(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(
+            backgroundColor: Colors.black,
+          );
+        } else if (snapshot.hasData && snapshot.data is User) {
+          return _buildUserDetails(snapshot.data as User);
+        } else {
+          return Text(snapshot.data, style: const TextStyle(color: Colors.white),);
+        }
+      },
+    );
+  }
+
+  Widget _buildUserDetails(User user) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.network(user.avatarUrl, height: 100,),
+          ),
+          Container(height: 10),
+          Text(
+            user.name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(height: 16),
+          _buildFollowersAndRepos(user),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFollowersAndRepos(User user) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildStat("Seguidores", user.followers, Colors.purple),
+        Container(width: 16),
+        _buildStat("Repositórios", user.publicRepos, Colors.deepOrange),
+      ],
+    );
+  }
+
+  Widget _buildStat(String label, int value, Color color) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      decoration: BoxDecoration(
+        color: color,
+      ),
+      child: Text(
+        "$label: $value",
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Future _fetchUser() async {
     final response = await http.get(Uri.parse('https://api.github.com/users/$username'));
 
     if (response.statusCode == 200) {
       return User.fromJson(json.decode(response.body));
     } else {
-      throw Exception('Ocorreu um erro');
+      return "Usuário $username inválido ou não encontrado.";
     }
-
   }
 }
