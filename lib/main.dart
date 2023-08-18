68e6b7128d8692acc07e39679247bf69a8c15707
Thalis Freitas <ofthalis@gmail.com>
2023-08-18 01:32:16 -0300

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Http Example',
+      title: 'Github Profile',
       home: HomeScreen(),
     );
   }
